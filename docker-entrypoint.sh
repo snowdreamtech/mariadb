511eabfaee35de2dc17dd87f827303dbf9573a2a
snowdream <sn0wdr1am@qq.com>
2024-08-08 23:35:55 +0800

    feat: bind-address

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ MARIADB_CONFIG_PATH=/etc/my.cnf.d/mariadb-server.cnf
 
 # Modifying configuration file mariadb-server.cnf
 # https://wiki.alpinelinux.org/wiki/MySQL
-sed -i "s|port\s*=\s*|port = ${MARIADB_PORT}|g" ${MARIADB_CONFIG_PATH}
+sed -i "s|port\s*=\s*.+|port = ${MARIADB_PORT}|g" ${MARIADB_CONFIG_PATH}
+sed -i "s|\#*bind-address\s*=.*|bind_address = 127.0.0.1,::1,db,mysql,mariadb|g" ${MARIADB_CONFIG_PATH}
 
 # Set default charset to UTF8MB4, in newer versions (since Alpine v3.9), 
 # just added a new file to added thus customization, 
