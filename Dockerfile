fb832bcea971299ddb69d5dbd74db4df927ff1ce
snowdream <sn0wdr1am@qq.com>
2024-08-09 20:08:34 +0800

    feat: mariadb-secure-installation

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,12 @@ LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 ENV MARIADB_VERSION=10.11.8-r0 \
     MARIADB_PORT=3306 \
     MARIADB_ROOT_PASSWORD="" \
+    MARIADB_DATABASE="" \
+    MARIADB_USER="" \
+    MARIADB_PASSWORD="" \
     REMOVE_ANONYMOUS_USERS=1 \
     DISALLOW_ROOT_LOGIN_REMOTELY=1 \
+    DISALLOW_USER_LOGIN_REMOTELY=1 \
     REMOVE_TEST_DATABASE=1 
 
 RUN apk add --no-cache mariadb=${MARIADB_VERSION} \
@@ -17,6 +21,6 @@ COPY mariadb-server.cnf /etc/my.cnf.d/mariadb-server.cnf
 
 EXPOSE 3306
 
-COPY docker-entrypoint.sh mariadb-setup.sh /usr/local/bin/
+COPY docker-entrypoint.sh mariadb-custom-installation mariadb-setup /usr/local/bin/
 
 ENTRYPOINT ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,21 @@ set -e
 
 MARIADB_CONFIG_PATH=/etc/my.cnf.d/mariadb-server.cnf
 
+
+# openssl rand -base64 33
+if [ -z "${MARIADB_ROOT_PASSWORD}" ]; then {
+    MARIADB_ROOT_PASSWORD=$(openssl rand -base64 33)
+    echo " Generate Random mariadb root password:${MARIADB_ROOT_PASSWORD}"
+}
+fi
+
 # Modifying configuration file mariadb-server.cnf
 # https://wiki.alpinelinux.org/wiki/MySQL
 sed -i "s|port\s*=\s*.+|port = ${MARIADB_PORT}|g" ${MARIADB_CONFIG_PATH}
-sed -i "s|\#*bind-address\s*=.*|bind_address = 127.0.0.1,::1,db,mysql,mariadb|g" ${MARIADB_CONFIG_PATH}
+# sed -i "s|\#*bind-address\s*=.*|bind_address = 127.0.0.1,::1,db,mysql,mariadb|g" ${MARIADB_CONFIG_PATH}
+#sed -i "s|\#*bind-address\s*=.*|bind_address = 0.0.0.0|g" ${MARIADB_CONFIG_PATH}
+#sed -i "/\#*skip-networking/d" ${MARIADB_CONFIG_PATH}
+
 
 # Set default charset to UTF8MB4, in newer versions (since Alpine v3.9), 
 # just added a new file to added thus customization, 
@@ -28,11 +39,15 @@ EOF
 # mysqld
 (
     sleep 3
-    /usr/local/bin/mariadb-setup.sh
+    /usr/local/bin/mariadb-setup
 ) &
 
 # mysqld
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+if [ ! -d "/var/lib/mysql" ];then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+else
+    echo "/var/lib/mysql exists."
+fi
 /usr/bin/mysqld_safe
 
 # exec commands
