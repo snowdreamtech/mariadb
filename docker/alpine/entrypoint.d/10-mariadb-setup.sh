1bf32f80da9c886918ebfd5b9c578705982e8780
snowdream <snowdream@users.noreply.github.com>
2026-06-23 08:10:01 +0800

    fix(docker): resolve expect script timeout and shell evaluation errors in mariadb setup scripts

--- docker/alpine/entrypoint.d/10-mariadb-setup.sh
+++ docker/alpine/entrypoint.d/10-mariadb-setup.sh
@@ -6,7 +6,7 @@ if [ "$DEBUG" = "true" ]; then echo "→ [mariadb] Setting up mariadb..."; fi
 # openssl rand -base64 33
 if [ -z "${MARIADB_ROOT_PWD}" ]; then
   {
-    MARIADB_ROOT_PWD=$(openssl rand -base64 33)
+    export MARIADB_ROOT_PWD=$(openssl rand -base64 33)
     echo "Generate Random mariadb root password:   ${MARIADB_ROOT_PWD}"
   }
 fi
@@ -15,7 +15,7 @@ fi
 # https://wiki.alpinelinux.org/wiki/MariaDB
 sed -i "s|port\s*=\s*.+|port = ${MARIADB_PORT}|g" /etc/my.cnf.d/mariadb-server.cnf
 # sed -i "s|\#*bind-address\s*=.*|bind_address = 127.0.0.1,::1,db,mariadb|g" /etc/my.cnf.d/mariadb-server.cnf
-if [ "${DISALLOW_ROOT_LOGIN_REMOTELY}" -eq 0 ] || [ "${DISALLOW_USER_LOGIN_REMOTELY}" -eq 0 ]; then
+if [ "${DISALLOW_ROOT_LOGIN_REMOTELY}" = "0" ] || [ "${DISALLOW_USER_LOGIN_REMOTELY}" = "0" ]; then
   sed -i "s|\#*bind-address\s*=.*|bind_address = 0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 fi
 sed -i "/\#*skip-networking/d" /etc/my.cnf.d/mariadb-server.cnf
